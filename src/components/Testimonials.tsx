f33acd85806e2dfe297018c79dd59a0595bc3973
Sunny Mishra <78862139+itsunny01-code@users.noreply.github.com>
2025-01-11 01:19:48 +0530

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,21 +4,26 @@ const stats = [
   {
     icon: "👥",
     value: "150+",
-    label: "in-house therapists & psychiatrists"
+    label: "Certified Professionals & Trainers"
   },
   {
     icon: "💫",
-    value: "6m+",
+    value: "10k+",
     label: "lives impacted"
   },
   {
     icon: "📅",
     value: "140k+",
-    label: "sessions delivered in therapy & psychiatry"
+    label: "Sessions delivered in counseling"
+  },
+  {
+    icon: "🏆",
+    value: "Best+",
+    label: "Counselling courses in India"
   },
   {
     icon: "👥",
-    value: "50,000+",
+    value: "20,000+",
     label: "community members"
   }
 ];
@@ -29,10 +34,10 @@ export const Testimonials = () => {
       <div className="container px-4 mx-auto max-w-[1200px]">
         <div className="max-w-4xl mx-auto text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold mb-4">
-            We've helped <span className="text-secondary">millions</span> feel better.
+            We've helped <span className="text-secondary">Thousands</span> feel better.
           </h2>
           <p className="text-white/70 text-lg">
-            Now it's your turn to benefit from the 'Amaha experience'...
+            Now it's your turn to benefit from the 'Endorphin experience'...
           </p>
         </div>
 
@@ -52,9 +57,9 @@ export const Testimonials = () => {
               />
             </div>
             <p className="text-base md:text-xl leading-relaxed mb-4">
-              "I was skeptical and afraid to be open to another person. I thought I could not do it, but my therapist has been very supportive in terms of my mental health journey and just allows me to feel my emotions and simplifies them for me. I trust her completely and know that she will be patient and understanding towards my problems."
+              "Endorphin.in has truly transformed my understanding of counseling. The courses are incredibly informative, easy to follow, and have practical applications. The team is supportive and always ready to help with any questions. Whether you're a beginner or looking to deepen your knowledge, these courses offer something for everyone. Highly recommend it to anyone serious about pursuing a career in counseling!"
             </p>
-            <p className="text-white/70">- Anonymous, Cabin Crew, Delhi</p>
+            <p className="text-white/70">- Anonymous, Pune</p>
           </div>
 
           <button className="absolute right-0 top-1/2 -translate-y-1/2 bg-white/10 p-3 rounded-full hidden md:block">
